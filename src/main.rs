431cb302979c34717d2be18d07d12eb0910ead3a
Lee Lup Yuen <luppy@appkaki.com>
2024-02-27 11:21:23 +0800

    Read disassembly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufReader, BufRead, Error};
+
+fn main() -> Result<(), Error> {
+    let input = File::open("/Users/Luppy/riscv/nuttx-tinyemu/docs/purescript/qjs.S")?;
+    let buffered = BufReader::new(input);
+
+    let mut count = 0;
+    for line in buffered.lines() {
+        println!("{}", line?);
+        count += 1;
+        if count > 10 { break; }
+    }
+
+    Ok(())
 }
