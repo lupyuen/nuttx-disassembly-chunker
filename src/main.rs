78a9c8fb0cd1f7cb4ebef42f4b6c85f1b71e86f5
Lee Lup Yuen <luppy@appkaki.com>
2024-02-27 13:12:14 +0800

    Chunking disassembly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::fs::File;
-use std::io::{BufReader, BufRead, Error};
+//! Split a NuttX Disassembly into Chunks for display by NuttX Log Parser in PureScript
+//! Need to chunk nuttx.S (or qjs.S) by address:
+//! nuttx-8000ad90.S, nuttx-8000ae00.S, nuttx-8000b000.S, nuttx-80010000.S
+//! Code Addresses are at 0x8000_0000 to 0x8006_4a28 (403 KB)
+//! Spanning 277K lines of code!
 
 // We dump the qjs disassembly to qjs.S
 // riscv64-unknown-elf-objdump \
@@ -10,6 +13,7 @@ use std::io::{BufReader, BufRead, Error};
 //   2>&1
 
 // qjs.S looks like this:
+// https://github.com/lupyuen/nuttx-tinyemu/blob/main/docs/purescript/qjs.S
 // 0000000080007028 <_start>:
 // _start():
 // /Users/Luppy/ox64/nuttx/arch/risc-v/src/common/crt0.c:166
@@ -17,6 +21,13 @@ use std::io::{BufReader, BufRead, Error};
 // {
 //     80007028:	1141                	addi	sp,sp,-16
 
+use std::fs::File;
+use std::io::{BufReader, BufRead, Error};
+
+/// Byte Size of a Single Chunk
+const CHUNK_SIZE: usize = 10 * 1024;  // 41 Files of 10 KB Chunks
+
+/// Split a NuttX Disassembly into Chunks for display by NuttX Log Parser in PureScript
 fn main() -> Result<(), Error> {
 
     // Open the NuttX Disassembly File
@@ -28,15 +39,17 @@ fn main() -> Result<(), Error> {
     let mut linenum = 0;
     for line in buffered.lines() {
         linenum += 1;
-        if linenum > 14_000 { break; }
+        if linenum > 40_000 { break; }
         let line = line?;
         println!("{}", line);
+
+        let chunk: usize = linenum / CHUNK_SIZE;
 
         // `addr` becomes `80007028`
         if let Some(cap) = re.captures_iter(&line).next() {
             if let Some(addr) = cap.get(1) {
                 let addr = addr.as_str();
-                println!("addr={}", addr);    
+                println!("chunk={}, addr={}", chunk, addr);    
             }
         }    
     }
